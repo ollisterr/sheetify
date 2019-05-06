911d99183e2ede3f6bf7150b3d1b33d8f81b8512
Olli Kiikkilä (Personal) <valkoinennauris@gmail.com>
2019-05-06 23:45:07 +0300

    Setupping up remove bbar function (not functional yet)

--- sheetify/src/sheetbody.js
+++ sheetify/src/sheetbody.js
@@ -2,42 +2,65 @@ import React, { useState } from 'react';
 import './css/sheetbody.css';
 import { timeSignature } from './state';
 
-const Bar = () => {
+const Bar = ({ id, removeBar }) => {    
     return (
         <div className="bar">
-            <input className="bar-block" />
-            <input className="bar-block" />
-            <input className="bar-block" />
-            <input className="bar-block" />
+            <div className="bar-controls">
+                <div className="add-bar-inbetween">+</div>
+                <div className="remove-bar" onClick={ () => {removeBar(id)} }>-</div>
+            </div>
+            <div className="bar-content">
+                <input className="bar-block" />
+                <input className="bar-block" />
+                <input className="bar-block" />
+                <input className="bar-block" />
+            </div>
         </div>
     )
 }
 
 const Section = () => {
-    const [bars, addBar] = useState([<Bar key="1" />]);
+    const [bars, setBarsState] = useState([
+        <Bar key="0" 
+            id="0" 
+            removeBar = { (id) => {
+                console.log("Remove: " + id);
+                setBarsState(bars.filter(bar => bar.props.id !== id));
+            } } 
+        />]);
 
+    const triggerAddBar = () => {
+        setBarsState([...bars, 
+            <Bar key={bars.length} 
+                id={bars.length}  
+                removeBar = { (id) => {
+                    console.log("Remove: " + id);
+                    setBarsState(bars.filter((bar) => {console.log(bar.props.id); return bar.props.id !== id;}));
+                } } 
+            />]);
+        console.log("Bars at the moment: " + bars.length);
+    }
+    
+    
     return (
         <div className="section">
             { bars }
-            <button className="add-bar" onClick={ () => { addBar([...bars, <Bar key={bars.length + 1}/>]) }} />
+            <button className="bar" onClick={ triggerAddBar } />
         </div>
     )
 }
 
-const ControlBar = () => {
-    return (
-        <div className="controlbar">
-            <button className="add-section" />
-            <button className="print" />
-        </div>
-    )
-}
 
 const SheetBody = ({ timeSignature }) => {
+    const [sections, addSection] = useState([<Section key="1" />]);
+
     return (
         <div className="sheetbody">
-            <Section />
-            <ControlBar />
+            { sections }
+            <div className="control-bar">
+                <button className="add-section" onClick={ () => { console.log("jhg"); addSection([...sections, <Section key={sections.length + 1}/>]) }} />
+                <button className="print" />
+            </div>
         </div>
     );
 }
