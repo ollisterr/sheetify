4f36d7955f9cb75e5fbfcd8847301993040721a1
Olli Kiikkilä <kiikkila.olli@gmail.com>
2020-01-08 15:38:29 +0100

    Bar goal bug fix

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -101,7 +101,6 @@ const Bar = ({ sectionID, barID }) => {
           onChange={setGoalName}
           placeholder='goal'
           tabIndex='-1'
-          data-html2canvas-ignore={bar.goal ? "true" : "false"}
         />
       </div>
       <div className='bar-content'>
