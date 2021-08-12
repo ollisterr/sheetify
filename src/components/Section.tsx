df3265c9b924e560de294e3e46fab4dc3f6877b1
Olli Kiikkilä <kiikkila.olli@gmail.com>
2021-08-12 13:55:26 +0300

    Build fixes

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import {
