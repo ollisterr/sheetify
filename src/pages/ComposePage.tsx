cdbf9ddc9d9759f066aa5362768bcc0378279792
Olli Kiikkilä <kiikkila.olli@gmail.com>
2021-08-24 11:34:44 +0300

    Load & save sheet

--- src/pages/ComposePage.tsx
+++ src/pages/ComposePage.tsx
@@ -1,7 +1,8 @@
+import React, { useRef, useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
-import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import styled from "styled-components";
+import axios from "axios";
 
 import ControlBar from "../components/ControlBar";
 import Section from "../components/Section";
@@ -11,12 +12,47 @@ import { Page } from "../styles";
 import { device } from "../utils/constants";
 
 const ComposePage: React.FC = observer(() => {
+  const [loading, setLoading] = useState(true);
   const printRef = useRef(null);
+
+  useEffect(() => {
+    const path = window.location.pathname.replace("/", "");
+
+    if (path.length > 0) {
+      axios.post("/.netlify/functions/load", { id: path }).then((res) => {
+        if (res.data) {
+          sheet.read(res.data);
+        }
+      }).finally(() => {
+        setLoading(false);
+      });
+    } else {
+      setLoading(false);
+    }
+  }, []);
 
   const printPDF = useReactToPrint({
     content: () => printRef.current,
     documentTitle: sheet.title
   });
+
+  const saveSheet = () => {
+    const path = window.location.pathname.replace("/", "");
+    setLoading(true);
+
+    axios.post(
+      "/.netlify/functions/save", 
+      { data: sheet, id: path.length ? path : undefined }
+    ).then((res) => {
+      const { id } = res.data;
+
+      if (id && id !== path) {
+        window.location.replace(`${window.location.origin}/${id}`);
+      }
+    }).finally(() => {
+      setLoading(false);
+    });
+  };
 
   return (
     <PageWrapper>
@@ -37,6 +73,8 @@ const ComposePage: React.FC = observer(() => {
       </SheetPaper>
 
       <ControlBar printPDF={printPDF} />
+
+      <button onClick={saveSheet}>SAVE SHEET</button>
     </PageWrapper>
   );
 });
