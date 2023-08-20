db8873d77392b7560f715a5ea59dc4cea23fb026
Olli Kiikkilä <kiikkila.olli@gmail.com>
2023-08-20 15:42:16 +0300

    fix: SSR style rendering

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,54 +1,85 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import Script from 'next/script';
 
 import { StyledComponentsRegistry } from '../lib/StyledComponentsRegistry';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="theme-color" content="#ffffff" />
+export default class DocumentPage extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-        <link
-          rel="shortcut icon"
-          sizes="180x180"
-          href="/sheetify-favicon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/sheetify-favicon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/sheetify-favicon.png"
-        />
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-        <link rel="shortcut icon" href="/sheetify-favicon.png" />
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 
-        <link
-          href="https://fonts.googleapis.com/css?family=Gaegu:400,700"
-          rel="stylesheet"
-        />
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#ffffff" />
 
-        <Script
-          data-domain="sheetify.kiikkila.com"
-          src="https://plausible.io/js/plausible.js"
-          async
-        />
-      </Head>
+          <link
+            rel="shortcut icon"
+            sizes="180x180"
+            href="/sheetify-favicon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/sheetify-favicon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/sheetify-favicon.png"
+          />
 
-      <body>
-        <StyledComponentsRegistry>
+          <link rel="shortcut icon" href="/sheetify-favicon.png" />
+
+          <link
+            href="https://fonts.googleapis.com/css?family=Gaegu:400,700"
+            rel="stylesheet"
+          />
+
+          <Script
+            data-domain="sheetify.kiikkila.com"
+            src="https://plausible.io/js/plausible.js"
+            async
+          />
+        </Head>
+
+        <body>
           <Main />
-        </StyledComponentsRegistry>
 
-        <NextScript />
-      </body>
-    </Html>
-  );
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
